46da625b786eb994ca55a31c0e85d9c82baf82b7
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-05-03 17:05:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+alert(`Bem vindo ao Mad calculator!`);
+
+function somar(n1,n2){
+    return  n1 + n2
+}
+
+function subtrair(n1,n2){
+    return  n1 - n2
+}
+
+
+var resultado = somar(4,7);
+console.log(`O resultado da soma é:${resultado}`);
+
+var resultadoSub = subtrair(3,2);
+console.log(`O resultado da subtração é:${resultadoSub}`)
